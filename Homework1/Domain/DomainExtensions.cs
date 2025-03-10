3322a82477ebac8e34d4de96627ebf8edee1fa75
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-11 00:07:42 +0300

    domain extensions

--- Homework1/Domain/DomainExtensions.cs
+++ Homework1/Domain/DomainExtensions.cs
@@ -1,6 +1,35 @@
-﻿namespace Fuse8.BackendInternship.Domain;
+﻿using System.Text;
+
+namespace Fuse8.BackendInternship.Domain;
 
 public static class DomainExtensions
 {
 	// ToDo: реализовать экстеншены
+
+	public static bool IsNullOrEmpty<T>(this IEnumerable<T>? items)
+	{
+		return items == null || !items.Any();
+	}
+
+	public static string JoinToString<T>(this IEnumerable<T>? items, string separator)
+	{
+		if (items.IsNullOrEmpty())
+			return string.Empty;
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		foreach (var item in items)
+		{
+			if (stringBuilder.Length > 0)
+				stringBuilder.Append(separator);
+			
+			stringBuilder.Append(item);
+		}
+
+		return stringBuilder.ToString();
+	}
+
+	public static int DaysCountBetween(this DateTimeOffset firstDateTimeOffset, DateTimeOffset secondDateTimeOffset)
+	{
+		return (firstDateTimeOffset.Date - secondDateTimeOffset.Date).Days;
+	}
 }

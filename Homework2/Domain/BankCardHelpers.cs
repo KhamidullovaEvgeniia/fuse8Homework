1f476b76348963037514c93787a3790727775fe8
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-17 19:05:40 +0300

    BankCardHelpers

--- Homework2/Domain/BankCardHelpers.cs
+++ Homework2/Domain/BankCardHelpers.cs
@@ -9,9 +9,23 @@ public static class BankCardHelpers
 	/// </summary>
 	/// <param name="card">Банковская карта</param>
 	/// <returns>Номер карты без маски</returns>
+
 	public static string GetUnmaskedCardNumber(BankCard card)
 	{
 		// ToDo: С помощью рефлексии получить номер карты без маски
-		return card.MaskedCardNumber;
+
+		var type = card.GetType();
+		var fieldInfo = type.GetField("_number", BindingFlags.NonPublic | BindingFlags.Instance);
+		
+		//TODO проверка на null
+		if (fieldInfo == null)
+			throw new Exception("Поле не должно быть пустым");
+			
+
+		return fieldInfo.GetValue(card).ToString();
+
+		//return card.MaskedCardNumber;
+
+
 	}
 }

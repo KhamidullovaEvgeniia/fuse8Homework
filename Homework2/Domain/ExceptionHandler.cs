4f2cfaf58ff5fc9d0cc7ca06a9744950d5dfde4e
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-17 19:05:50 +0300

    ExceptionHandler

--- Homework2/Domain/ExceptionHandler.cs
+++ Homework2/Domain/ExceptionHandler.cs
@@ -4,41 +4,78 @@ namespace Fuse8.BackendInternship.Domain;
 
 public static class ExceptionHandler
 {
-	/// <summary>
-	/// Обрабатывает исключение, которое может возникнуть при выполнении <paramref name="action"/>
-	/// </summary>
-	/// <param name="action">Действие, которое может породить исключение</param>
-	/// <returns>Сообщение об ошибке</returns>
-	public static string? Handle(Action action)
-	{
-		// ToDo: Реализовать обработку исключений
-		action();
-		return "Ok";
-	}
+    /// <summary>
+    /// Обрабатывает исключение, которое может возникнуть при выполнении <paramref name="action"/>
+    /// </summary>
+    /// <param name="action">Действие, которое может породить исключение</param>
+    /// <returns>Сообщение об ошибке</returns>
+    public static string? Handle(Action action)
+    {
+        // ToDo: Реализовать обработку исключений
+        // action();
+        //
+        // return "Ok";
+
+        try
+        {
+            action();
+            return null;
+        }
+        catch (NotValidKopekCountException ex)
+        {
+            return ex.Message;
+        }
+        catch (NegativeRubleCountException ex)
+        {
+            return ex.Message;
+        }
+        catch (MoneyException ex)
+        {
+            return ex.Message;
+        }
+        catch (HttpRequestException ex)
+        {
+            if (ex.StatusCode is HttpStatusCode.NotFound)
+                return "Ресурс не найден";
+            
+            return ex.StatusCode.ToString();
+        }
+        catch (Exception)
+        {
+            return "Произошла непредвиденная ошибка";
+        }
+
+        
+    }
 }
 
 public class MoneyException : Exception
 {
-	public MoneyException()
-	{
-	}
+    public MoneyException()
+    {
+    }
 
-	public MoneyException(string? message)
-		: base(message)
-	{
-	}
+    public MoneyException(string? message) : base(message)
+    {
+    }
 }
 
 public class NotValidKopekCountException : MoneyException
 {
-	public NotValidKopekCountException()
-	{
-	}
+    //TODO как лучше писать через переменную или сразу
+    private const string DefaultMessage = "Количество копеек должно быть больше 0 и меньше 99";
+
+    public NotValidKopekCountException() : base(DefaultMessage)
+    {
+    }
 }
 
 public class NegativeRubleCountException : MoneyException
 {
-	public NegativeRubleCountException()
-	{
-	}
+    //TODO как выше
+    private const string DefaultMessage = "Число рублей не может быть отрицательным";
+
+    public NegativeRubleCountException() : base(DefaultMessage)
+    {
+    }
 }

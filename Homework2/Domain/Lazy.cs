da2dcc02fa72311513eab8e243a546cd469783d3
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-17 19:06:08 +0300

    Lazy

--- Homework2/Domain/Lazy.cs
+++ Homework2/Domain/Lazy.cs
@@ -5,7 +5,30 @@
 /// </summary>
 public class Lazy<TValue>
 {
-	// ToDo: Реализовать ленивое получение значение при первом обращении к Value
+    // ToDo: Реализовать ленивое получение значение при первом обращении к Value
 
-	public TValue? Value { get; }
+    private readonly Func<TValue> _func;
+
+    private bool _isCreated;
+
+    private TValue? _value;
+
+    public Lazy(Func<TValue> func)
+    {
+        _func = func;
+    }
+
+    public TValue? Value
+    {
+        get
+        {
+            if (!_isCreated)
+            {
+                _value = _func();
+                _isCreated = true;
+            }
+
+            return _value;
+        }
+    }
 }

855bddf52814a946565a9ee35bae18b4835af585
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-17 19:05:31 +0300

    AssemblyHelpers

--- Homework2/Domain/AssemblyHelpers.cs
+++ Homework2/Domain/AssemblyHelpers.cs
@@ -4,49 +4,68 @@ namespace Fuse8.BackendInternship.Domain;
 
 public static class AssemblyHelpers
 {
-	/// <summary>
-	/// Получает информацию о базовых типах классов из namespace "Fuse8.BackendInternship.Domain", у которых есть наследники.
-	/// </summary>
-	/// <remarks>
-	///	Информация возвращается только по самым базовым классам.
-	/// Информация о промежуточных базовых классах не возвращается
-	/// </remarks>
-	/// <returns>Список типов с количеством наследников</returns>
-	public static (string BaseTypeName, int InheritorCount)[] GetTypesWithInheritors()
-	{
-		// Получаем все классы из текущей Assembly
-		var assemblyClassTypes = Assembly.GetAssembly(typeof(AssemblyHelpers))
-			!.DefinedTypes
-			.Where(p => p.IsClass);
+    /// <summary>
+    /// Получает информацию о базовых типах классов из namespace "Fuse8.BackendInternship.Domain", у которых есть наследники.
+    /// </summary>
+    /// <remarks>
+    ///	Информация возвращается только по самым базовым классам.
+    /// Информация о промежуточных базовых классах не возвращается
+    /// </remarks>
+    /// <returns>Список типов с количеством наследников</returns>
+    private static readonly string Namespace = "Fuse8.BackendInternship.Domain";
 
-		// ToDo: Добавить реализацию
-		throw new NotImplementedException();
-	}
+    public static (string BaseTypeName, int InheritorCount)[] GetTypesWithInheritors()
+    {
+        // Получаем все классы из текущей Assembly
+        var assemblyClassTypes =
+            Assembly.GetAssembly(typeof(AssemblyHelpers)) !.DefinedTypes.Where(p => p.IsClass && p.Namespace == Namespace);
 
-	/// <summary>
-	/// Получает базовый тип для класса
-	/// </summary>
-	/// <param name="type">Тип, для которого необходимо получить базовый тип</param>
-	/// <returns>
-	/// Первый тип в цепочке наследований. Если наследования нет, возвращает null
-	/// </returns>
-	/// <example>
-	/// Класс A, наследуется от B, B наследуется от C
-	/// При вызове GetBaseType(typeof(A)) вернется C
-	/// При вызове GetBaseType(typeof(B)) вернется C
-	/// При вызове GetBaseType(typeof(C)) вернется C
-	/// </example>
-	private static Type? GetBaseType(Type type)
-	{
-		var baseType = type;
+        // ToDo: Добавить реализацию
 
-		while (baseType.BaseType is not null && baseType.BaseType != typeof(object))
-		{
-			baseType = baseType.BaseType;
-		}
+        var baseTypesWithInheritors = new Dictionary<Type, HashSet<Type>>();
+        foreach (var type in assemblyClassTypes)
+        {
+            if (type.IsAbstract)
+                continue;
 
-		return baseType == type
-			? null
-			: baseType;
-	}
+            var baseClass = GetBaseType(type);
+            if (baseClass == null)
+                continue;
+
+            if (baseClass.Namespace != Namespace)
+                continue;
+
+            if (baseTypesWithInheritors.ContainsKey(baseClass))
+                baseTypesWithInheritors[baseClass].Add(type);
+            else
+                baseTypesWithInheritors[baseClass] = new HashSet<Type> { type };
+        }
+
+        return baseTypesWithInheritors.Select(kvp => (BaseTypeName: kvp.Key.Name, InheritorCount: kvp.Value.Count)).ToArray();
+    }
+
+    /// <summary>
+    /// Получает базовый тип для класса
+    /// </summary>
+    /// <param name="type">Тип, для которого необходимо получить базовый тип</param>
+    /// <returns>
+    /// Первый тип в цепочке наследований. Если наследования нет, возвращает null
+    /// </returns>
+    /// <example>
+    /// Класс A, наследуется от B, B наследуется от C
+    /// При вызове GetBaseType(typeof(A)) вернется C
+    /// При вызове GetBaseType(typeof(B)) вернется C
+    /// При вызове GetBaseType(typeof(C)) вернется C
+    /// </example>
+    private static Type? GetBaseType(Type type)
+    {
+        var baseType = type;
+
+        while (baseType.BaseType is not null && baseType.BaseType != typeof(object))
+        {
+            baseType = baseType.BaseType;
+        }
+
+        return baseType == type ? null : baseType;
+    }
 }

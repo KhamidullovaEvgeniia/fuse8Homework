cd2990aefb617f302f7792c7d0cb00a81ff10c6b
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-18 14:51:29 +0300

    BankCardHelpers fix

--- Homework2/Domain/BankCardHelpers.cs
+++ Homework2/Domain/BankCardHelpers.cs
@@ -4,28 +4,27 @@ namespace Fuse8.BackendInternship.Domain;
 
 public static class BankCardHelpers
 {
-	/// <summary>
-	/// Получает номер карты без маски
-	/// </summary>
-	/// <param name="card">Банковская карта</param>
-	/// <returns>Номер карты без маски</returns>
+    /// <summary>
+    /// Получает номер карты без маски
+    /// </summary>
+    /// <param name="card">Банковская карта</param>
+    /// <returns>Номер карты без маски</returns>
+    public static string GetUnmaskedCardNumber(BankCard card)
+    {
+        // ToDo: С помощью рефлексии получить номер карты без маски
 
-	public static string GetUnmaskedCardNumber(BankCard card)
-	{
-		// ToDo: С помощью рефлексии получить номер карты без маски
+        if (card == null)
+            throw new ArgumentNullException(nameof(card));
 
-		var type = card.GetType();
-		var fieldInfo = type.GetField("_number", BindingFlags.NonPublic | BindingFlags.Instance);
-		
-		//TODO проверка на null
-		if (fieldInfo == null)
-			throw new Exception("Поле не должно быть пустым");
-			
+        var type = card.GetType();
+        var fieldInfo = type.GetField("_number", BindingFlags.NonPublic | BindingFlags.Instance);
 
-		return fieldInfo.GetValue(card).ToString();
+        if (fieldInfo == null)
+            throw new ArgumentNullException(message: "Поле не должно быть пустым", paramName: nameof(fieldInfo));
 
-		//return card.MaskedCardNumber;
-
-
-	}
+        return fieldInfo.GetValue(card)?.ToString()
+               ?? throw new ArgumentNullException(
+                   message: "Значение не должно быть пустым",
+                   paramName: nameof(fieldInfo.GetValue));
+    }
 }

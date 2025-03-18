4fb16161c6b0e77fb5bb64eadf4d0730850df1f0
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-18 14:13:31 +0300

    DomainExtensions

--- Homework1/Domain/DomainExtensions.cs
+++ Homework1/Domain/DomainExtensions.cs
@@ -13,23 +13,11 @@ public static class DomainExtensions
 
 	public static string JoinToString<T>(this IEnumerable<T>? items, string separator)
 	{
-		if (items.IsNullOrEmpty())
-			return string.Empty;
-		
-		StringBuilder stringBuilder = new StringBuilder(items.Count() * 2);
-		foreach (var item in items)
-		{
-			if (stringBuilder.Length > 0)
-				stringBuilder.Append(separator);
-			
-			stringBuilder.Append(item);
-		}
-
-		return stringBuilder.ToString();
+		return items is not null ? string.Join(separator, items) : string.Empty;
 	}
 
 	public static int DaysCountBetween(this DateTimeOffset firstDateTimeOffset, DateTimeOffset secondDateTimeOffset)
 	{
-		return (firstDateTimeOffset.Date - secondDateTimeOffset.Date).Days;
+		return Math.Abs((firstDateTimeOffset.Date - secondDateTimeOffset.Date).Days);
 	}
 }

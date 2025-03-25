971454730e70f25749fad892f4489584f34e390f
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-03-25 17:31:34 +0300

    Exception

---
+++ CurrencyApi/PublicApi/Exceptions/ApiRequestLimitException.cs
@@ -0,0 +1,16 @@
+﻿namespace Fuse8.BackendInternship.PublicApi.Exceptions;
+
+public class ApiRequestLimitException : Exception
+{
+    public ApiRequestLimitException()
+    {
+    }
+
+    public ApiRequestLimitException(string message) : base(message)
+    {
+    }
+
+    public ApiRequestLimitException(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+}
---
+++ CurrencyApi/PublicApi/Filters/ExceptionFilter.cs
@@ -0,0 +1,35 @@
+﻿using Fuse8.BackendInternship.PublicApi.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Fuse8.BackendInternship.PublicApi.Filters;
+
+public class ExceptionFilter : IExceptionFilter
+{
+    private readonly ILogger<ExceptionFilter> _logger;
+
+    public ExceptionFilter(ILogger<ExceptionFilter> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is ApiRequestLimitException)
+        {
+            _logger.LogError(context.Exception, "Too Many Requests");
+            context.Result = new ObjectResult("Too Many Requests") { StatusCode = StatusCodes.Status429TooManyRequests };
+        }
+        else if (context.Exception is CurrencyNotFoundException)
+        {
+            context.Result = new ObjectResult("Currency Not Found") { StatusCode = StatusCodes.Status404NotFound };
+        }
+        else
+        {
+            _logger.LogError(context.Exception, "Internal Server Error");
+            context.Result = new ObjectResult("Internal Server Error") { StatusCode = StatusCodes.Status500InternalServerError };
+        }
+
+        context.ExceptionHandled = true;
+    }
+}

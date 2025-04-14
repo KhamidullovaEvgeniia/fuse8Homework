a0846a667d41cae57f54a29f54d86e3154f7187c
KhamidullovaEvgeniia <140321071+KhamidullovaEvgeniia@users.noreply.github.com>
2025-04-15 00:18:22 +0300

    ef configurations

---
+++ CurrencyApi/PublicApi.DataAccess/Configurations/FavoriteCurrencyRateConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PublicApi.DataAccess.Models;
+
+namespace PublicApi.DataAccess.Configurations;
+
+public class FavoriteCurrencyRateConfiguration : IEntityTypeConfiguration<FavoriteCurrencyRate>
+{
+    public void Configure(EntityTypeBuilder<FavoriteCurrencyRate> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.HasIndex(x => x.Name).IsUnique();
+
+        builder.HasIndex(x => new { x.Currency, x.BaseCurrency }).IsUnique();
+
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+
+        builder.HasCheckConstraint("CK_Currency_NotEqual", "\"currency\" <> \"base_currency\"");
+    }
+}
--- CurrencyApi/PublicApi.DataAccess/FavoriteCurrencyDbContext.cs
+++ CurrencyApi/PublicApi.DataAccess/FavoriteCurrencyDbContext.cs
@@ -5,37 +5,18 @@ namespace PublicApi.DataAccess;
 
 public class FavoriteCurrencyDbContext : DbContext
 {
+    public DbSet<FavoriteCurrencyRate> FavoriteCurrencyRates { get; set; }
+
+    public const string SchemaName = "user";
+
     public FavoriteCurrencyDbContext(DbContextOptions<FavoriteCurrencyDbContext> options) : base(options)
     {
     }
 
-    public DbSet<FavoriteCurrencyRate> FavoriteCurrencyRates { get; set; }
-    
-    public const string SchemaName = "user";
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.HasDefaultSchema(SchemaName);
-
-        modelBuilder.Entity<FavoriteCurrencyRate>(
-            entity =>
-            {
-                entity.HasKey(e => e.Id);
-                
-                entity.HasIndex(x => x.Name).IsUnique();
-
-                entity
-                    .HasIndex(
-                        x => new
-                        {
-                            x.Currency,
-                            x.BaseCurrency
-                        })
-                    .IsUnique();
-
-                entity.Property(x => x.Name).IsRequired().HasMaxLength(100);
-
-                entity.HasCheckConstraint("CK_Currency_NotEqual", "\"currency\" <> \"base_currency\"");
-            });
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(FavoriteCurrencyDbContext).Assembly);
     }
 }
